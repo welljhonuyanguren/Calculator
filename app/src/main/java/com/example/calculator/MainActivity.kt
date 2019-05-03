e046d3530e477fa47699380a29420b5fe0323cf1
welljhon <welljhonguyanguren@gmail.com>
2019-05-03 16:03:12 +0800

    learning to make an android app

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -12,89 +12,125 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        updateUI(mainUIString= "")
+
+    }
+        var dotPresent: Boolean = false
+        var operationNumber: String= ""
+        var caNumber:String = ""
+        var screenNumber: Int = 0
+        var ansNumber: Int= 0
+        var secNumber: Int= 0
+        var performingOpe: CharSequence = ""
+        var performing: Boolean = false
+        var equal : Boolean = false
 
 
-        back.setOnClickListener {
+
+    fun numberClick(view: View){
+        if (equal == false) {
+            if (performing == false) {
+                val button = view as Button
+                val numString = button.text
+
+                displayExpression.text = displayExpression.text.toString() + numString
+//        operationNumber = displayExpression.text.toString()
+            } else {
+                val button = view as Button
+                val numString = button.text
+
+                displayResult.text = displayResult.text.toString() + numString
+//          caNumber = displayResult.text.toString()
+            }
+        }
+
+        else {
+            displayExpression.text = ""
+            displayResult.text = ""
+            dotPresent= false
+            performing = false
+            equal == false
+            operationNumber =""
+            caNumber = ""
+            screenNumber= 0
+            ansNumber= 0
+        }
+    }
+
+    fun dotClick(view: View) {
+        if (dotPresent == false) {
+            if (performing == false) {
+                val button = view as Button
+                val numString = button.text
+
+                displayExpression.text = displayExpression.text.toString() + numString
+                operationNumber = displayExpression.text.toString()
+            } else {
+                val button = view as Button
+                val numString = button.text
+
+                displayResult.text = displayResult.text.toString() + numString
+                caNumber = displayResult.text.toString()
+            }
+            dotPresent = true
+        }
+    }
+
+
+    fun operatorclick(view: View){
+        val button = view as Button
+        performingOpe = button.text
+        ansNumber = operationNumber.toInt()
+        performing = true
+
+
+    }
+
+
+
+    fun clearAll(view: View){
+        displayExpression.text = ""
+        displayResult.text = ""
+        dotPresent= false
+        performing = false
+        equal == false
+        operationNumber =""
+        caNumber = ""
+        screenNumber= 0
+        ansNumber= 0
+    }
+
+    fun del(view: View){
+
             val string = displayExpression.text.toString()
             if (string.isNotEmpty()){
                 displayExpression.text = string.substring(0,string.length-1)
             }
-        }
-
-        dot.setOnClickListener{
-            if(dotPresent == false){
-                displayExpression.text = displayExpression.text.toString() +"."
-                dotPresent= true }
-        }
-
-    }
-        var dotPresent: Boolean = false
-        val operationNumber: MutableList<String> = arrayListOf()
-        val caNumber: MutableList<String> = arrayListOf()
-        var screenNumber: Int= 0
-        var ansNumber: Int= 0
-        var performingNum: Boolean = false
-
-
-
-
-
-
-    private fun extractString (items: List<String>, connect:String = ""): String {
-        if(items.isEmpty()) return ""
-        return items.reduce {acc, s -> acc + connect + s}
-    }
-
-    private fun updateUI(mainUIString: String){
-        val calculationString= extractString(operationNumber, connect= "" )
-        var calculationText = findViewById<View>(R.id.displayExpression) as TextView
-
-        calculationText.text = calculationString
-
-        val ans = findViewById<View>(R.id.displayResult) as TextView
-        ans.text = mainUIString
-    }
-
-    fun numberClick(view: View){
-        val button = view as Button
-        val numString = button.text
-
-        caNumber.add(numString.toString())
-        val text = extractString(caNumber)
-        updateUI(text)
-
-    }
-
-    fun operatorclick(view: View){
-        val button = view as Button
-        if (caNumber.isEmpty()) return
-
-        operationNumber.add(extractString(caNumber))
-        caNumber.clear()
-      //  operationNumber.add(button.text.toString())
-        updateUI(button.text.toString())
-    }
-
-    private fun clearview(){
-        operationNumber.clear()
-        caNumber.clear()
-        dotPresent= false
-    }
-
-    fun clearAll(view: View){
-        clearview()
-        updateUI(mainUIString= "")
     }
 
     fun equalClick(){
-        operationNumber.add(extractString(caNumber))
-        caNumber.clear()
+        secNumber = caNumber.toInt()
+        if (performingOpe == "+"){
+            ansNumber +=  secNumber
+        }
+        else if (performingOpe == "-"){
+            ansNumber -= secNumber
+        }
+        else if (performingOpe == "*"){
+            ansNumber *= secNumber
+        }
+        else if (performingOpe == "÷"){
+            ansNumber /= secNumber
+        }
+        displayResult.text = ansNumber.toString()
+        displayExpression.text = ""
+        equal = true
 
-        val calculator = calculatorString()
-        val answer = calculator.calculate(operationNumber)
-        updateUI(mainUIString= "=" + answer.toString())
-        clearview()
+    }
+    fun percentClick(){
+        ansNumber /= 100
+        displayResult.text = ansNumber.toString()
+        displayExpression.text = ""
+        equal = true
     }
 
 }
